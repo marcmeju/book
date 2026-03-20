53e573775a61fc3c5e40266cc9f7b7401d66069f
marcmeju <marcelmejulu@gmail.com>
2026-03-20 01:00:54 -0700

    add 'get book' and 'get all books' methods

--- routes.py
+++ routes.py
@@ -5,14 +5,15 @@ book_blueprint = Blueprint('book_api_routes', __name__, url_prefix='/api/book')
 
 
 @book_blueprint.route('/', methods=['GET'])
-def get_books():
+def get_homepage():
     return "Welcome to the Book API!"
 
 @book_blueprint.route('/all', methods=['GET'])
 def get_all_books():
-#    books = Book.query.all()
-#    return jsonify([book.to_dict() for book in books]), 200
-    return "All books"
+     books = Book.query.all()
+     result = jsonify([book.serialize() for book in books])
+     response = {"Books": result}
+     return jsonify(response)
 
 @book_blueprint.route('/create', methods=['POST'])
 def create_book():
@@ -36,6 +37,8 @@ def create_book():
 
 @book_blueprint.route('/<int:book_isbn>', methods=['GET'])
 def get_book(book_isbn):
-    # book = Book.query.get_or_404(book_id)
-    # return jsonify(book.to_dict()), 200
-    return f"Found - Book with ISBN {book_isbn}"
+    book = Book.query.filter_by(isbn=book_isbn).first()
+    if book:
+        return jsonify(book.serialize())
+    else:
+        return jsonify({"Message": f"Book with ISBN {book_isbn} not found"})
